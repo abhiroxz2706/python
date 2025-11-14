458dfba3e7dfc40f3433cde8740fca0c8b6a3ba3
abhiroxz <abhiroxz2706@gmail.com>
2025-11-14 18:03:46 +0530

    lists

---
+++ lists/activity 1.py
@@ -0,0 +1,10 @@
+lst=[] #empty list
+print(lst)
+lst=[1,56.3,"hello",7,2]
+print(lst)
+print(lst[2])
+print(lst[1:3])
+print(lst*3)
+print("Reverse of a list:")
+lst.reverse()
+print(lst)
---
+++ lists/activity 2.py
@@ -0,0 +1,8 @@
+data=[27,77,46,45,97]
+sum=0
+for i in data:
+    sum=sum+i
+print("total=",sum)
+print("average=",sum/(len(data)))
+print("maximum number:",max(data))
+print("minimum number:",min(data))
