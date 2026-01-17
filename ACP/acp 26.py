87c816ca6ba543955d015db5f69db7085549261d
abhiroxz <abhiroxz2706@gmail.com>
2026-01-17 14:00:39 +0530

    acp 27

---
+++ ACP/acp 26.py
@@ -0,0 +1,8 @@
+Set1={"blue","green"}
+Set2={"blue","yellow"}
+
+print("the set symmetric difference of set 1 and set 2",Set1.symmetric_difference(Set2))
+
+set_1={1,2,3,4,5}
+set_2={1,5,6,7,8,9}
+print("the set symmetric difference of set 1 and set 2",set_1.symmetric_difference(set_2))
---
+++ ACP/acp 27.py
@@ -0,0 +1,13 @@
+tup1=(4,3,2,2,-1,18)
+product=1
+
+for i in tup1:
+    product=product*i
+    print("the product of a all the numbers in tuple 1 are:",product)
+
+tup2=(2,4,8,8,3,2,9)
+p=1
+
+for i in tup2:
+    p=p*i
+    print("the product of a all the numbers in tuple 2 are:",p)
